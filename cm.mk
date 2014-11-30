5f149b1442f74cb64edde7638b696ca038c86f5a
barracuda7 <barracudaking@gmail.com>
2014-11-30 20:52:06 +1100

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -11,3 +11,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=klimtwifi \
     PRODUCT_DEVICE=klimtwifi \
     TARGET_DEVICE=klimtwifi \
+    BUILD_FINGERPRINT="samsung/klimtwifixx/klimtwifi:4.4.2/KOT49H/T700XXU1ANF9:user/release-keys" \
+    PRIVATE_BUILD_DESC="klimtwifixx-user 4.4.2 KOT49H T700XXU1ANF9 release-keys"
